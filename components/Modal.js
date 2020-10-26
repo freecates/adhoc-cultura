adf012fbec9738028c57dfe8cb95aeb149228c7e
Ramon Gil <ramon.gil@deporvillage.com>
2020-10-26 09:39:52 +0100

    master, button 'ara' restyled

--- components/Modal.js
+++ components/Modal.js
@@ -11,21 +11,24 @@ const Modal = () => (
         <h2>
           <img src={'/static/aracultura.png'} />
         </h2>
-        <p style={{textAlign:'center'}}>
+        <p style={{ textAlign: 'center' }}>
           <a
+            title={'Anar a Ara Cultura'}
             style={{
               background: '#56b34a',
               color: '#ffffff',
               textDecoration: 'none',
               padding: '.5rem',
+              display: 'block',
+              textTransform: 'uppercase',
             }}
             href={'https://www.aracultura.com/'}>
-            Explora, participa i utilitza
+            Explora
           </a>
         </p>
         <p>
-          les propostes que ha desenvolupat Adhoc Cultura per l'Àmbit cultural
-          en resposta a la situació de pandèmia.
+          participa i utilitza les propostes que ha desenvolupat Adhoc Cultura
+          per l'Àmbit cultural en resposta a la situació de pandèmia.
         </p>
       </div>
     )}
